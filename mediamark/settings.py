a430dff6ad7c8a269669e8c45fac6629ee3341d8
CristianTacoronteRivero <cristian@cristian-gl552vwrog.home>
2022-10-27 12:37:28 +0100

    feat: ejemplo scrapy y generacion csv

--- mediamark/settings.py
+++ mediamark/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'mediamark.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
+USER_AGENT = 'Mozilla/6.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
 
 # SCRAPOXY
 # CONCURRENT_REQUESTS_PER_DOMAIN = 1
@@ -43,7 +43,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- mediamark/spiders/laptops.py
+++ mediamark/spiders/laptops.py
@@ -1,4 +1,22 @@
 import scrapy
+from numpy import nan
+from datetime import datetime
+#  scrapy shell -s USER_AGENT='custom user agent' 'http://www.example.com'
+
+def precios_item(extract):
+    """Separa el precio anterior y actual del item seleccionado.
+    Si solo tiene un precio, devuelve ese y como segundo valor devuelve Not a Number
+
+    :param extract: Contiene los precios del item
+    :type extract: List[str]
+    :return: Devuelve los precios del item por separado
+    :rtype: float
+    """
+    if len(extract) > 1:
+        return float(extract[0]), float(extract[1])
+    else:
+        return nan, float(extract[0])
+
 
 
 class LaptopsSpider(scrapy.Spider):
@@ -7,8 +25,17 @@ class LaptopsSpider(scrapy.Spider):
     start_urls = ['https://www.mediamarkt.es/es/category/port%C3%A1tiles-153.html']
 
     def parse(self, response):
-        title = response.xpath('//h1/text()').get()
+        items = response.xpath('//*[@data-test="mms-search-srp-productlist-item"]')
+        fecha = fecha = datetime.now().strftime('%Y/%m/%d')
 
-        yield {
-            '-------- titulo -------':title
-        }
+        for item in items:
+            modelo = item.xpath('.//p/text()').get()
+            precios = item.xpath('.//span[2]/text()').extract()
+            precio_anterior, precio_actual = precios_item(precios)
+
+            yield {
+                'fecha':fecha,
+                'modelo':modelo,
+                'precio_anterior':precio_anterior,
+                'precio_actual':precio_actual
+            }
